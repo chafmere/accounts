50071860e635f595ad019fd79ce19f4ddd20b67d
isaac <isaac_1990@hotmail.com>
2020-07-23 07:28:17 +1000

    Updated License information

--- src/main.c
+++ src/main.c
@@ -1,3 +1,21 @@
+/* licence-window.c
+ *
+ * Copyright 2020 Isaac Asley
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 
 #include "buildtutorlia-config.h"
 
@@ -18,7 +36,7 @@ int main(int argc, char **argv) {
   gtk_init (&argc, &argv);
 
   builder = gtk_builder_new_from_file ("/home/chaff/Projects/buildtutorlia/src/mainwindow.glade");
-  sqlBuilder = gtk_builder_new_from_file ("/home/chaff/Projects/buildtutorlia/src/sqlbox.glade"); //TODO merger both glade files. This is for testing purposes only.
+  sqlBuilder = gtk_builder_new_from_file ("/home/chaff/Projects/buildtutorlia/src/sqlbox.glade"); //TODO merge both glade files. This is for testing purposes only.
 
   window = GTK_WIDGET(gtk_builder_get_object(builder, "main_window"));
 
